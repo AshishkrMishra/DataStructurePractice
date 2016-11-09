7b222a7bcdd1c43e9bb1a9c5ef2d45ec89e3bf09
Ashish Kumar Mishra <ashish.103022@gmail.com>
2016-11-10 02:31:04 +0530

    This is just Demo Junit Case To see wether dependecy is getting Added or not

--- src/test/java/com/akm/test/linkedlist/sdaaameij/DemoTest.java
+++ src/test/java/com/akm/test/linkedlist/sdaaameij/DemoTest.java
@@ -1,12 +1,16 @@
 package com.akm.test.linkedlist.sdaaameij;
 
 
+import org.junit.Test;
+
 /**
  * Created by ashishkumarmishra on 11/10/16.
  */
 public class DemoTest {
+    @Test
     public void runTest()
     {
+        System.out.print("From Here we will start");
 
     }
 }
